aca16917f4c7cfb40de744e2f487fbd466cd0045
BriSC60962 <70978684+BriSC60962@users.noreply.github.com>
2021-04-13 20:58:56 -0300

    Initial commit

---
+++ Assets/Animation1.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class Animation1 : MonoBehaviour
+{
+    public GameObject Timeline;
+    private void OnTriggerEnter(Collider other)
+    {
+        Timeline.GetComponent<PlayableDirector>().Play();
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        gameObject.SetActive(false);
+    }
+
+}
---
+++ Assets/Animation2.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class Animation2 : MonoBehaviour
+{
+    public GameObject Fire;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Fire.GetComponent<PlayableDirector>().Play();
+    }
+}
